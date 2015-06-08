fbb1bb2b280c3712c11a74182e6f95221610ec31
Noppol Pilukruangdet <knookcs08@gmail.com>
2015-06-08 15:28:09 +0700

    guestbook data source

--- GuestBook_Data/GuestBookDataSource.cs
+++ GuestBook_Data/GuestBookDataSource.cs
@@ -36,5 +36,31 @@ namespace GuestBook_Data
             table.Execute(operation);
         }
 
+        public IEnumerable<GuestBookEntry> GetGuestBookEntries()
+        {
+            CloudTable table = tableClient.GetTableReference("GuestBookEntry");
+            TableQuery<GuestBookEntry> query = new TableQuery<GuestBookEntry>();
+            query = query.Where(TableQuery.GenerateFilterCondition(
+                "PartitionKey", QueryComparisons.Equal, 
+                DateTime.UtcNow.ToString("MMddyyyy")));
+            return table.ExecuteQuery(query);
+        }
+
+        public void UpdateImageThumbnail(string partitionKey, string rowKey,
+            string thumbUrl)
+        {
+            CloudTable table = tableClient.GetTableReference("GuestBookEntry");
+            TableOperation retrieveOperation = TableOperation.Retrieve<GuestBookEntry>(
+                partitionKey, rowKey);
+            TableResult result = table.Execute(retrieveOperation);
+            GuestBookEntry updateEntity = (GuestBookEntry)result.Result;
+            if (updateEntity != null)
+            {
+                updateEntity.ThumbnailUrl = thumbUrl;
+                TableOperation replaceOperation = TableOperation.Replace(updateEntity);
+                table.Execute(replaceOperation);
+            }
+        }
+
     }
 }
