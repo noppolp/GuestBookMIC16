5ab14ee33dc344b4350b2e638a028913be29032d
Noppol Pilukruangdet <knookcs08@gmail.com>
2015-06-08 16:18:08 +0700

    InitializeStorage

--- GuestBook_WebRole/Default.aspx.cs
+++ GuestBook_WebRole/Default.aspx.cs
@@ -5,13 +5,57 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.IO;
+using System.Net;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using GuestBook_Data;
+
 namespace GuestBook_WebRole
 {
-    public partial class _Default : Page
+    public partial class _Default : System.Web.UI.Page
     {
+        private static bool storageInitialized = false;
+        private static CloudBlobClient blobStorage;
+        private static CloudQueueClient queueStorage;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+			if (!Page.IsPostBack)
+			{
+				this.Timer1.Enabled = true;
+			}
+        }
 
+        protected void SignButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+			this.DataList1.DataBind();
+        }
+
+        private void InitializeStorage()
+        {
+            if (!storageInitialized)
+            {
+                var storageAccount = CloudStorageAccount.Parse(
+                        CloudConfigurationManager.GetSetting("DataConnectionString"));
+                blobStorage = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobStorage.GetContainerReference(
+                    "guestbookpics");
+                container.CreateIfNotExists();
+
+                var permissions = container.GetPermissions();
+                permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+                container.SetPermissions(permissions);
+
+                storageInitialized = true; 
+            }
         }
     }
 }
